e5b8662fee1be76adeb26f3afc217524b60484a4
Nageshjagtap <nageshjagtap063@gmail.com>
2025-07-10 15:42:47 +0530

    add the cercular code

---
+++ cercular.cpp
@@ -0,0 +1,61 @@
+
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class list {
+    Node* head;
+    Node* tail;
+
+public:
+    list() {
+        head = tail = NULL;
+    }
+
+    void push(int val) {
+        Node* newNode = new Node(val);
+        if (head == NULL) {
+            head = tail = newNode;
+            tail->next = head;
+        } else {
+            newNode->next = head;
+            head = newNode;
+            tail->next = head;
+        }
+    }
+
+    void printList() {
+        if (head == NULL) {
+            cout << "List is empty.\n";
+            return;
+        }
+
+        Node* temp = head;
+        do {
+            cout << temp->data << "->";
+            temp = temp->next;
+        } while (temp != head);
+        cout << " head" << endl;
+    }
+};
+ 
+int main() {
+    list cll;
+    cll.push(1);
+    cll.push(2);
+    cll.push(3);
+
+    cll.printList();
+
+    return 0;
+}
